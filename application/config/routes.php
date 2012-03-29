4507fc36d5fbd7f5026a2f87aa66ea279442e895
unknown <lamli@CASSOULET.etudiants.efrei.fr>
2012-03-29 12:44:59 +0200

    news

--- application/config/routes.php
+++ application/config/routes.php
@@ -38,6 +38,8 @@
 |
 */
 
+$route['news/(:any)'] = 'news/view/$1';
+$route['news'] = 'news';
 $route['default_controller'] = 'pages/view';
 $route['(:any)'] = 'pages/view/$1';
 $route['404_override'] = '';
