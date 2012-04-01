d47bfab1ef25c218575e2ea17a47df6e4367e287
Liv LAM <chealiv@gmail.com>
2012-04-01 19:11:23 +0200

    Test merge

---
+++ application/models/news_model.php
@@ -0,0 +1,21 @@
+<?php
+	class News_Model extends CI_Model
+	{
+		public function __construct()
+		{
+			$this->db->load->database();
+		}
+		
+		public function get_news($slug = false)
+		{
+			if ($slug === false)
+			{
+				$query = $this->db->get('news');
+				return $query->result_array();
+			}
+			
+			$query = $this->db->get_where('news', array('slug' => $slug));
+			return $query->row_array();
+		}
+	}
+?>
